6b2479c27a39ce352cfcb7f68b8103a5d7aed0ee
Stefanut99009 <stefanutpazargic@gmail.com>
2024-07-30 18:33:18 +0300

    ceva

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import { PageProps } from '@/types';
+import { PageProps, User } from '@/types';
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -13,7 +13,7 @@ export default function Dashboard({ auth }: PageProps) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="p-6 text-gray-900">Welcome {auth.user.name}</div>
                     </div>
                 </div>
             </div>
